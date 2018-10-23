72a46565e6d7591abbcfdf978cc139115e10cfb7
Kyösti Ranto <kyosti.ranto@eficode.com>
2018-10-23 23:25:59 +0300

    refactored dataloader

--- public/data/data.js
+++ public/data/data.js
@@ -2397,4 +2397,62 @@ fdata = {
   }
 }
 
+function prepareData(data) {
+  Object.keys(data.mun).forEach(function(key) {
+    var m = data.mun[key];
+    m.name = m.name? m.name: key;
+  });
+
+  Object.keys(data.piers).forEach(function(key) {
+    var pier = data.piers[key];
+    pier.id = key;
+    pier.name = pier.name? pier.name: key;
+    pier.mun = data.mun[pier.mun];
+    pier.type = pier.type? pier.type: "2";
+  });
+
+  Object.keys(data.routes).forEach(function(key) {
+    var route = data.routes[key];
+    route.id = key;
+    route.piers = route.piers.map(function(pier) {
+      return data.piers[pier];
+    });
+    route.vessels = route.vessels.map(function(vessel) {
+      return data.ferries[vessel];
+    });
+
+    if (!route.operator) route.operator = [];
+    if (!Array.isArray(route.operator)) route.operator = [route.operator];
+    route.operator = route.operator.map(function(op) { return data.operators[op] });
+    route.pricelists = data.pricelists[route.pricelists];
+
+    if (!Array.isArray(route.timetables)) route.timetables = [route.timetables];
+
+    route.timetables = route.timetables.map(function(timetable) {
+      data.timetables[timetable].id = timetable;
+      return data.timetables[timetable];
+    });
+
+  });
+
+  var i = 0;
+  data.lauttaRoutes.forEach(function(lr) { lr.id = i++; });
+
+  Object.keys(data.mun).forEach(function(key) { addFinnishInessiivi(data.mun[key]); });
+}
+
+function addFinnishInessiivi(mun) {
+  var vokaalit = "aeiouyåäö";
+  var etuvokaalit = "yäö";
+  if (mun.name_fi_in) return;
+  var name = mun.name_fi || mun.name;
+  var lastChar = name.slice(-1);
+  if (vokaalit.indexOf(lastChar) < 0) name += "i";
+  var isEtu = etuvokaalit.split("").map(function(ev) { return name.indexOf(ev) >= 0; }).reduce(function(x, y) { return x || y; }, false);
+  name += isEtu? "ssä": "ssa";
+  mun.name_fi_in = name;
+}
+
+prepareData(fdata);
+
 console.log(JSON.stringify(fdata));
