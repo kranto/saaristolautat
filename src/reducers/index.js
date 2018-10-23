16edf205f3cf6e1e3882e90f39c52d18576be81d
Kyösti Ranto <kyosti.ranto@eficode.com>
2018-10-23 21:21:46 +0300

    removed vesselReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
-import vessels from "./vesselReducer";
 import settings from "./settingsReducer";
 
 export default combineReducers({
-	vessels,
 	settings
 })
