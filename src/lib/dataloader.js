a1e6a532fcd99a11041f01815a0708b198bde2a0
Kyösti Ranto <kyosti.ranto@eficode.com>
2018-10-25 23:08:12 +0300

    dataloader loads first index.json

--- src/lib/dataloader.js
+++ src/lib/dataloader.js
@@ -1,15 +1,25 @@
 import axios from 'axios';  
 
-const geo1P = axios.get('/data/saaristo.json?v=1.5');
-const geo2P = axios.get('/data/roads.json?v=1');
-const geo3P = axios.get('/data/routes.json?v=1');
-const dataP = axios.get('/data/data.json?v=1.24');
+const baseUri = '/data/';
+const indexUri = baseUri + 'index.json?v=' + (Math.random() + "").substring(2);
+const indexP = axios.get(indexUri);
+
+let allPs;
+
+indexP.then(({data: indexData}) => {
+  const dataP = axios.get(baseUri + indexData.data);
+  const geoPs = indexData.geojson.map(uri => axios.get(baseUri + uri));
+  allPs = geoPs.slice();
+  allPs.unshift(dataP);  
+});
 
 export function loadFerriesData(callback) {
-
-  Promise.all([dataP, geo1P, geo2P, geo3P])
-  .then(([{data}, {data: geo1}, {data: geo2}, {data: geo3}]) => {
-    callback(data, [geo1, geo2, geo3]);
+  indexP.then(() => {
+    Promise.all(allPs)
+    .then((responses) => {
+      const data = responses.shift().data;
+      const geos = responses.map(response => response.data);
+      callback(data, geos);
+    });  
   });
-
 }
