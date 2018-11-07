313f22b6f72faca92ae4b46c042ecf69ebe46476
Kyösti Ranto <kyosti.ranto@eficode.com>
2018-11-07 23:22:37 +0200

    refactored settingsReducer: whole settings are stored to and read from localstorage

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,39 +1,58 @@
-const localStorgageLayers = localStorage.getItem("layers");
-
-export const layers = localStorgageLayers ? JSON.parse(localStorgageLayers) : {
-  ringroads: false,
-  distances: true,
-  roadferries: true,
-  conn4: true,
-  conn5: false,
-  longdistanceferries: false,
-  live: false,
+let initialSettings = getFromLocalStorage("settings") ||
+{
+  layers: {
+    ringroads: false,
+    distances: true,
+    roadferries: true,
+    conn4: true,
+    conn5: false,
+    longdistanceferries: false,
+    live: false,
+  },
+  locale: window.navigator.language.split("-")[0] || "fi",
+  mapTypeId: 'roadmap'
 };
 
-localStorage.setItem("layers", JSON.stringify(layers));
+initialSettings = {...initialSettings, isFullScreen: false};
 
-const initialLocale = localStorage.getItem('language') || window.navigator.language.split("-")[0] || "fi";
-localStorage.setItem('language', initialLocale);
+setToLocalStorage("settings", initialSettings);
 
-export default function reducer(state = {
-  locale: initialLocale,
-  isFullScreen: false,
-  layers: layers,
-  mapTypeId: 'roadmap'
-}, action) {
+export default function reducer(state = initialSettings, action) {
+  const newState = handleAction(state, action);
+  setToLocalStorage("settings", newState);
+  return newState;
+}
+
+function handleAction(state, action) {
   switch (action.type) {
     case "LOCALE_SET":
-      localStorage.setItem('language', action.payload);
+      setToLocalStorage('language', action.payload);
       return { ...state, locale: action.payload };
     case "FULLSCREEN_CHANGED":
       return { ...state, isFullScreen: action.payload };
     case "LAYER_SET":
       const layers = Object.assign({}, state.layers, action.payload);
-      localStorage.setItem("layers", JSON.stringify(layers));
+      setToLocalStorage("layers", JSON.stringify(layers));
       return { ...state, layers: layers };
     case "MAP_TYPE_SELECTED":
       return { ...state, mapTypeId: action.payload };
     default:
       return state;
+  }
+}
+
+function getFromLocalStorage(item) {
+  if (typeof Storage !== 'undefined') {
+    const value = localStorage.getItem(item);
+    if (typeof value !== 'undefined') {
+      return JSON.parse(value);
+    }
+  }
+  return null;
+}
+
+function setToLocalStorage(item, value) {
+  if (typeof Storage !== 'undefined') {
+    localStorage.setItem(item, JSON.stringify(value));
   }
 }
