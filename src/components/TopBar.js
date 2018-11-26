bc5d4a2301d9aa8921e9545195543a80a43e1e49
Kyösti Ranto <kyosti.ranto@eficode.com>
2018-11-26 18:13:37 +0200

    removed some obsolete definitions

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -6,14 +6,14 @@ export default class TopBar extends Component {
   render() {
     return (
       <div id="topbar">
-        <button id="menubutton" type="button" className={"topbarbutton" + (this.props.menuOpen ? " active" : "")} onClick={this.props.onMenuButtonClicked}>
+        <button type="button" className={"topbarbutton" + (this.props.menuOpen ? " active" : "")} onClick={this.props.onMenuButtonClicked}>
           <i className="fa fa-bars" aria-hidden="true"></i>
           <span className="description">{L2("topbar.info")}</span>
         </button>
         <div id="topbartitle">
           {L2("application.title")}
         </div>
-        <button id="settingsbutton" type="button" className={"topbarbutton right" + (this.props.settingsOpen ? " active" : "")} 
+        <button type="button" className={"topbarbutton right" + (this.props.settingsOpen ? " active" : "")} 
           onClick={this.props.onSettingsButtonClicked}>
           <span className="description">{L2("topbar.settings")}</span><i className="fa fa-sliders" aria-hidden="true"></i>
         </button>
