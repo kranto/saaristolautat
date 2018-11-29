cb939f3b6edcaa8dd08e59c073e819f483e534e9
Kyösti Ranto <kyosti.ranto@eficode.com>
2018-11-29 23:30:53 +0200

    live indicator shown only after introduction phase

--- src/components/LiveIndicator.js
+++ src/components/LiveIndicator.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { L2 } from '../lib/localizer';
+import { phases } from '../lib/constants';
 
 class LiveIndicator extends Component {
 
   render() {
-    const msg = this.props.live.msg ? L2(this.props.live.msg) : "";
+    const msg = this.props.live.msg && this.props.phase >= phases.NORMAL_USE ? L2(this.props.live.msg) : "";
     const showMsg = msg || L2("live.closing");
     return (
       <div id="liveind" className={msg ? "visible" : "hidden"}>
@@ -18,7 +19,8 @@ class LiveIndicator extends Component {
 const mapStateToProps = (state) => {
   return {
     locale: state.settings.locale,
-    live: state.live
+    live: state.live,
+    phase: state.uiState.phase
   };
 };
 
