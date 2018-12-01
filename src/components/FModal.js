18f298c939f8f961ae74e982dccdf7f38434f72c
Kyösti Ranto <kyosti.ranto@eficode.com>
2018-12-01 15:01:41 +0200

    closeInfoButton is now sticky also in fmodal

--- src/components/FModal.js
+++ src/components/FModal.js
@@ -19,11 +19,13 @@ export default class FModal extends Component {
   contents() {
     return (
       <div id={this.props.id} className="fmodal" onClick={this.onCloseClicked.bind(this)} key="1" ref={this.fmodalRef}>
+      <div class="closeInfoButtonHolder">
+      <button type="button" className="btn btn-secondary closeInfoButton" onClick={this.onCloseClicked.bind(this)}>
+          <i className="fa fa-times" aria-hidden="true"></i>
+        </button>
+      </div>
         <div className="fmodalcontent" onClick={this.stopPropagation}>
-          <div className="fmodalheader" style={{position: "relative"}}>
-          <button type="button" className="btn btn-secondary closeInfoButton" onClick={this.onCloseClicked.bind(this)}>
-            <i className="fa fa-times" aria-hidden="true"></i>
-          </button>
+          <div className="fmodalheader" style={{ position: "relative" }}>
             {this.props.header}
           </div>
           <div className="fmodalbody">
@@ -31,7 +33,7 @@ export default class FModal extends Component {
           </div>
         </div>
       </div>
-      );  
+    );
   }
 
   render() {
@@ -42,7 +44,7 @@ export default class FModal extends Component {
       <ReactCSSTransitionGroup transitionName="fmodal"
         transitionEnterTimeout={500}
         transitionLeaveTimeout={300}>
-        {this.props.show ? this.contents() : "" }
+        {this.props.show ? this.contents() : ""}
       </ReactCSSTransitionGroup>
     );
   }
