c3d5b048c0ca8c6ad697b4bb293a83689adb4736
Kyösti Ranto <kyosti.ranto@cargotec.com>
2023-01-21 16:03:22 +0200

    updated live scripts to use new ais API

--- scripts/update_vessels.sh
+++ scripts/update_vessels.sh
@@ -4,12 +4,12 @@ DIR=$(dirname $0)
 
 TARGET=/home3/meigyale/livedata
 
-python $DIR/get_vessels10.py $TARGET/lauttalive.db True #update history
+python $DIR/get_vessels12.py $TARGET/lauttalive.db True #update history
 mv livedata.json1 $TARGET/livedata.json
 mv livehistory.json1 $TARGET/livehistory.json
 
 sleep 27
 
-python $DIR/get_vessels10.py $TARGET/lauttalive.db False #do not update history
+python $DIR/get_vessels12.py $TARGET/lauttalive.db False #do not update history
 mv livedata.json1 $TARGET/livedata.json
 
